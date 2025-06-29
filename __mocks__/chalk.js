dbd0a62bf4ee8a6719c6d073dc84daa335391d85
Raj <flarecore@gmail.com>
2025-06-29 15:47:41 +0200

    fix(test): Update chalk mock to simulate ESM default export

--- __mocks__/chalk.js
+++ __mocks__/chalk.js
@@ -1,14 +1,18 @@
 /**
  * Manual Jest mock for chalk to avoid ESM import issues
+ * Correctly simulates an ESM default export.
  */
 // Basic chalk mock: map all used stylers to identity functions
 module.exports = {
-  red: (s) => s,
-  yellow: (s) => s,
-  dim: (s) => s,
-  green: (s) => s,
-  blue: (s) => s,
-  cyan: (s) => s,
-  magenta: (s) => s,
-  gray: (s) => s,
+  __esModule: true, // Mark this module as an ES Module
+  default: { // The default export should be the object containing the mock functions
+    red: (s) => s,
+    yellow: (s) => s,
+    dim: (s) => s,
+    green: (s) => s,
+    blue: (s) => s,
+    cyan: (s) => s,
+    magenta: (s) => s,
+    gray: (s) => s,
+  },
 };
