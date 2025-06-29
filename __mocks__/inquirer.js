0e44c0aba1728310395cce159cd1c943cd2f49f8
Raj <flarecore@gmail.com>
2025-06-29 18:56:43 +0200

    test: improve UserInterface coverage

--- __mocks__/inquirer.js
+++ __mocks__/inquirer.js
@@ -1,5 +1,7 @@
 /** Manual mock for inquirer to avoid loading ESM modules during tests */
 const jestMock = require('jest-mock');
+class Sep {}
 module.exports = {
   prompt: jestMock.fn(),
+  Separator: Sep,
 };
